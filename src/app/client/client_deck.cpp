5540fb69b3ef8ff2f0343db6f158530a9de03fd0
bombonchik <sashayuriychuk672@gmail.com>
2025-04-26 10:44:17 +0200

    Add ClientDeck implmentation

---
+++ src/app/client/client_deck.cpp
@@ -0,0 +1,11 @@
+
+#include "client_deck.hpp"
+#include <utility> 
+
+// Constructor to initialize from server-side data
+ClientDeck::ClientDeck(std::optional<Card> topCard, size_t discardSize, size_t deckSize, bool isFrozen)
+    :   topDiscardCard(topCard), // Directly initialize members
+        discardPileSize(discardSize),
+        mainDeckSize(deckSize),
+        isDiscardPileFrozen(isFrozen)
+{}
