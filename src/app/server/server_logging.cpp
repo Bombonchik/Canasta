e23d1147300dded27650d5ef278292a4c1f33f91
bombonchik <sashayuriychuk672@gmail.com>
2025-04-28 12:31:17 +0200

    Fix logging pattern

--- src/app/server/server_logging.cpp
+++ src/app/server/server_logging.cpp
@@ -75,8 +75,8 @@ void initLogger()
         "server", spdlog::sinks_init_list{console_sink, info_sink, err_sink},
         spdlog::thread_pool(), spdlog::async_overflow_policy::block
     );
-    logger->set_level(spdlog::level::info);
+    logger->set_level(lvl::info);
     spdlog::set_default_logger(logger);
-    spdlog::set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%^%l%$] [%n] %v");
+    spdlog::set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%n] [%^%l%$] %v");
     spdlog::flush_every(std::chrono::seconds(2));
 }
